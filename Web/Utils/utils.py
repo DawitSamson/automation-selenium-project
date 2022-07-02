66c6598d27c311f1f08bf14c537339bfe95ecced
Your Name <you@example.com>
2022-07-02 15:35:13 +0300

    update utils file

--- Web/Utils/utils.py
+++ Web/Utils/utils.py
@@ -43,6 +43,7 @@ class Utils:
     def city_name_incorrectly(self):
         return self.driver.find_element(By.XPATH, self.errorCityName).get_attribute('innerText')
 
+    @allure.step
     def navbar_links_list(self):
         return self.driver.find_elements(By.XPATH, self.navBarLinks)
 
@@ -71,9 +72,11 @@ class Utils:
 
             driver.back()
 
+    @allure.step
     def click_accessibility_button(self):
         self.driver.find_element(By.CSS_SELECTOR, self.accessibilityButton).click()
 
+    @allure.step
     def click_colors(self):
         wait = WebDriverWait(self.driver, 20)
         colors = self.driver.find_elements(By.XPATH, self.accessibilityColors)
@@ -86,15 +89,15 @@ class Utils:
                 colors[color].click()
                 self.driver.implicitly_wait(10)
 
-                """ verify the background is change to the correct color """
-                self.assertion(colors[0].value_of_css_property('background-image'),
-                               'linear-gradient(120deg, rgb(0, 0, 0) 40%, rgb(245, 222, 179) 60%)')
-                self.assertion(colors[1].value_of_css_property('background-image'),
-                               'linear-gradient(120deg, rgb(245, 203, 92) 40%, rgb(36, 36, 35) 60%)')
-                self.assertion(colors[2].value_of_css_property('background-image'),
-                               'linear-gradient(120deg, rgb(20, 33, 61) 40%, rgb(152, 193, 217) 60%)')
-                self.assertion(colors[3].value_of_css_property('background-image'),
-                               'linear-gradient(120deg, rgb(0, 0, 0) 40%, rgb(152, 150, 241) 60%)')
+        """ verify the background is change to the correct color """
+        self.assertion(colors[0].value_of_css_property('background-image'),
+                       'linear-gradient(120deg, rgb(0, 0, 0) 40%, rgb(245, 222, 179) 60%)')
+        self.assertion(colors[1].value_of_css_property('background-image'),
+                       'linear-gradient(120deg, rgb(245, 203, 92) 40%, rgb(36, 36, 35) 60%)')
+        self.assertion(colors[2].value_of_css_property('background-image'),
+                       'linear-gradient(120deg, rgb(20, 33, 61) 40%, rgb(152, 193, 217) 60%)')
+        self.assertion(colors[3].value_of_css_property('background-image'),
+                       'linear-gradient(120deg, rgb(0, 0, 0) 40%, rgb(152, 150, 241) 60%)')
 
 
 
