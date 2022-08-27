00eea6f27a811143ec30c75fe93bd9648ff0deed
Your Name <you@example.com>
2022-08-27 14:50:22 +0300

    - add footer tests - continue with userProfile - conftest in Tests folder - Pytest - run command from terminal('browser') - add to conftest (links, search, accessibility, footer) - Run on Jenkins - Load Tests with locust - API Tests added, PUT and DELETE

--- Mobile/first_test_Mobile.py
+++ Mobile/first_test_Mobile.py
@@ -34,13 +34,14 @@ driver = webdriver.Remote(
 search_element = WebDriverWait(driver, 30).until(
     EC.element_to_be_clickable((MobileBy.ACCESSIBILITY_ID, "Search Wikipedia"))
 )
+a = driver.find_element(MobileBy.WINDOWS_UI_AUTOMATION)
 search_element.click()
 search_input = WebDriverWait(driver, 30).until(
     EC.element_to_be_clickable((MobileBy.ID, "org.wikipedia.alpha:id/search_src_text"))
 )
 search_input.send_keys("BrowserStack")
 time.sleep(5)
-search_results = driver.find_elements_by_class_name("android.widget.TextView")
+search_results = driver.find_elements(MobileBy.CLASS_NAME, "android.widget.TextView")
 assert (len(search_results) > 0)
 # Invoke driver.quit() after the test is done to indicate that the test is completed.
 driver.quit()
